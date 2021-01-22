2ff3fe2c5d90beddeff58b4d9771050fb60288fa
Masaki0509 <76939051+Masaki0509@users.noreply.github.com>
2021-01-22 20:15:16 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText; 
+
+    //スコアのカウント用
+    private int score; 
+
+    void Scorepoint(int score)
+    {
+        this.scoreText.GetComponent<Text>().text = "Score" + this.score;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        this.scoreText = GameObject.Find("ScoreText");
+
+        score = 0;
+        Scorepoint(score);   //初期スコアを代入して表示
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if (tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.score += 100;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.score += 50;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            this.score += 200;
+        }
+
+        Scorepoint(score);
+    }
+}
+
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoreText; //Text用変数
+    private int score = 0; //スコア計算用変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        SetScore();   //初期スコアを代入して表示
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string tag = collision.gameObject.tag;
+    
+        if (tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.score += 100;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            this.score += 50;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            this.score += 200;
+        }
+
+        SetScore();
+
+    }
+
+    void SetScore()
+    {
+        scoreText.text = string.Format("Score:{0}", score);
+    }
+}
+
